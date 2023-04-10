d48abe959d0bafc5540945ab0a32dfdbc04df682
Ian <ian.thomas@tiani-spirit.com>
2023-04-10 20:44:16 +0100

    new file:   main 	modified:   main.go 	modified:   main.zip

--- main.go
+++ main.go
@@ -48,20 +48,27 @@ func Handle_Request(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyR
 			trans.Pathway = value
 		case tukcnst.TUK_EVENT_QUERY_PARAM_NHS:
 			trans.NHS_ID = value
+		case tukcnst.QUERY_PARAM_USER:
+			trans.User = value
+		case tukcnst.QUERY_PARAM_ORG:
+			trans.Org = value
+		case tukcnst.QUERY_PARAM_ROLE:
+			trans.Role = value
 		}
 	}
 	if req.HTTPMethod == http.MethodPost {
-		switch task {
-		case "def":
-			trans.Actor = tukcnst.XDW_ADMIN_REGISTER_DEFINITION
-		case "meta":
-			trans.Actor = tukcnst.XDW_ADMIN_REGISTER_XDS_META
-		}
 		trans.DSUB_BrokerURL = os.Getenv(tukcnst.ENV_DSUB_BROKER_URL)
-		trans.DSUB_ConsumerURL = os.Getenv(tukcnst.ENV_DB_USER)
+		trans.DSUB_ConsumerURL = os.Getenv(tukcnst.ENV_DSUB_CONSUMER_URL)
 		trans.Request = []byte(req.Body)
-		tukxdw.Execute(&trans)
-		if task == "meta" {
+		switch task {
+		case "regdef":
+			trans.Actor = tukcnst.XDW_ADMIN_REGISTER_DEFINITION
+			tukxdw.Execute(&trans)
+			trans.Actor = tukcnst.XDW_ADMIN_REGISTER_XDS_META
+			tukxdw.Execute(&trans)
+		case "regmeta":
+			trans.Actor = tukcnst.XDW_ADMIN_REGISTER_XDS_META
+			tukxdw.Execute(&trans)
 			return queryResponse(http.StatusOK, "", tukcnst.TEXT_PLAIN)
 		}
 	}
@@ -76,7 +83,7 @@ func Handle_Request(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyR
 }
 func setAwsResponseHeaders(contentType string) map[string]string {
 	awsHeaders := make(map[string]string)
-	awsHeaders["Server"] = "TUK_XDW_Consumer_Proxy"
+	awsHeaders["Server"] = "TUK_XDW_Creator_Proxy"
 	awsHeaders["Access-Control-Allow-Origin"] = "*"
 	awsHeaders["Access-Control-Allow-Headers"] = "accept, Content-Type"
 	awsHeaders["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
